8c7ed9552219f346c3fef5e2011f5a094fbe505e
SulaymonGit <buuusuuu7@gmail.com>
2025-02-22 10:52:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const harif = ["nok", "banan", "mashina", "asal", "olma", "gul", "non"];
+
+const letters = "nhrif";
+
+
+function containsNhrif(hariflar) {
+  for (let i = 0; i < hariflar.length; i++) {
+    if (letters.includes(hariflar[i])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+const wordsWithNhrif = [];
+const wordsWithoutNhrif = [];
+
+
+harif.forEach(harif => {
+  if (containsNhrif(harif)) {
+    wordsWithNhrif.push(harif);
+  } else {
+    wordsWithoutNhrif.push(harif);
+  }
+});
+
+
+console.log("n harifi mavjut:", wordsWithNhrif);
+console.log(" n harifi mavjut emas:", wordsWithoutNhrif);
